af4476c0647fb57aa9a690211ef4eed9f65e26a4
Giang Nguyen <giang.nguyen@tiki.vn>
2018-08-29 10:27:22 +0700

    deleted v1 to create clean v2

--- app/src/main/java/vn/tiki/android/androidhometest/di/Factory.kt
+++
@@ -1,3 +0,0 @@
-package vn.tiki.android.androidhometest.di
-
-typealias Factory<T> = () -> T
--- app/src/main/java/vn/tiki/android/androidhometest/util/Assets.kt
+++
@@ -1,8 +0,0 @@
-package vn.tiki.android.androidhometest.util
-
-import android.content.res.AssetManager
-import java.nio.charset.Charset
-
-fun AssetManager.readFile(file: String): String {
-  return open(file).bufferedReader(Charset.forName("utf-8")).use { it.readText() }
-}
